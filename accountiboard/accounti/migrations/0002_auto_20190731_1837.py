a765495bfaf95879d88cd33aaaf19e56a45f523c
imanshafiei <imanshafiei@ymail.com>
2019-07-31 18:42:07 +0430

    bug fix.

--- accountiboard/accounti/migrations/0002_auto_20190731_1837.py
+++ accountiboard/accounti/migrations/0002_auto_20190731_1837.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AddField(
-            model_name='menuitem',
+            model_name='MenuItem',
             name='is_delete',
             field=models.SmallIntegerField(default=0),
         ),
