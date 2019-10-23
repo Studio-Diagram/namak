9f2d70f6b35822f10fe48b454eb934baa16d78ed
imanshafiei <imanshafiei@ymail.com>
2019-10-23 12:32:13 +0330

    showing only last 100 items in accounti section.

--- accountiboard/accounti/views/InvoiceReturnView.py
+++ accountiboard/accounti/views/InvoiceReturnView.py
@@ -199,7 +199,7 @@ def get_all_invoices(request):
             return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
 
         branch_obj = Branch.objects.get(pk=branch_id)
-        invoice_objects = InvoiceReturn.objects.filter(branch=branch_obj).order_by('-id')
+        invoice_objects = InvoiceReturn.objects.filter(branch=branch_obj).order_by('-id')[:100]
         invoices = []
         for invoice in invoice_objects:
             invoice_date = invoice.created_time.date()
