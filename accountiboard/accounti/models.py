8b6457d1a421ed1cee211cca6c2b8320b353104d
imanshafiei <imanshafiei@ymail.com>
2020-04-25 18:24:08 +0430

    Bugs fix for migration step 4 and final.

--- accountiboard/accounti/models.py
+++ accountiboard/accounti/models.py
@@ -209,7 +209,7 @@ class Cash(models.Model):
     outcome_report = models.IntegerField(null=False, default=0)
     event_tickets = models.IntegerField(null=False, default=0)
     current_money_in_cash = models.IntegerField(null=False, default=0)
-    new_employee = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    employee = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
     branch = models.ForeignKey(to=Branch, on_delete=models.CASCADE)
     is_close = models.SmallIntegerField(default=0, null=False)
 
@@ -452,7 +452,7 @@ class DeletedItemsInvoiceSales(models.Model):
     item_type = models.CharField(max_length=50, choices=ITEM_TYPES)
     item_numbers = models.IntegerField(null=False)
     message = models.CharField(max_length=255)
-    new_employee = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+    employee = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
     invoice_sales = models.ForeignKey(to=InvoiceSales, on_delete=models.SET_NULL, null=True)
 
 
