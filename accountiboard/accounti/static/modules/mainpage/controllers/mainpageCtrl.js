ad9bf10b2454f4285906d8cb408ad028cb1cc3ca
imanshafiei <imanshafiei@ymail.com>
2020-08-04 12:49:29 +0430

    new register UI add.

---
+++ accountiboard/accounti/static/modules/mainpage/controllers/mainpageCtrl.js
@@ -0,0 +1,8 @@
+angular.module("mainpage")
+    .controller("mainpageCtrl", function ($scope, $interval, $rootScope, $filter, $http, $state, $auth, $timeout, $window, mainpageHttpRequest) {
+        var initialize = function () {
+            $state.go('login');
+        };
+
+        initialize();
+    });
