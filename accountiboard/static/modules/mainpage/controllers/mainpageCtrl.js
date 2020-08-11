86dfd90eaaee6912871dfdb0b411c3cb26603d71
Sohrab Alimardani <serpent_code@protonmail.com>
2020-08-11 14:10:20 +0430

    removed static folder

--- accountiboard/static/modules/mainpage/controllers/mainpageCtrl.js
+++
@@ -1,8 +0,0 @@
-angular.module("mainpage")
-    .controller("mainpageCtrl", function ($scope, $interval, $rootScope, $filter, $http, $state, $auth, $timeout, $window, mainpageHttpRequest) {
-        var initialize = function () {
-            $state.go('login');
-        };
-
-        initialize();
-    });
