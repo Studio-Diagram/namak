420c89f05e3bd4aba58fbf8142b74f8da1701dea
Sohrab Alimardani <serpent_code@protonmail.com>
2020-09-01 19:47:13 +0430

    classbase refactored TableView.py

--- accountiboard/accounti/views/TableView.py
+++ accountiboard/accounti/views/TableView.py
@@ -3,10 +3,15 @@ import json
 from accounti.models import *
 from accountiboard.constants import *
 from django.db import IntegrityError
+from django.views import View
 
 
-def add_table(request):
-    if request.method == "POST":
+class AddTableView(View):
+    # @permission_decorator_class_based(token_authenticate,
+    #     {USER_ROLES['CAFE_OWNER']},
+    #     {USER_PLANS_CHOICES['FREE']},
+    #     branch_disable=False)
+    def post(self, request, *args, **kwargs):
         rec_data = json.loads(request.read().decode('utf-8'))
         table_id = rec_data['table_id']
         table_cat_id = rec_data['table_cat_id']
@@ -45,11 +50,13 @@ def add_table(request):
 
             return JsonResponse({"response_code": 2})
 
-    return JsonResponse({"response_code": 4, "error_msg": "GET REQUEST!"})
 
-
-def search_table(request):
-    if request.method == "POST":
+class SearchTableView(View):
+    # @permission_decorator_class_based(token_authenticate,
+    #     {USER_ROLES['CAFE_OWNER']},
+    #     {USER_PLANS_CHOICES['FREE']},
+    #     branch_disable=False)
+    def post(self, request, *args, **kwargs):
         rec_data = json.loads(request.read().decode('utf-8'))
         search_word = rec_data['search_word']
         username = rec_data['username']
@@ -66,11 +73,14 @@ def search_table(request):
                 'table_cat_name': table.category.name,
             })
         return JsonResponse({"response_code": 2, 'tables': tables})
-    return JsonResponse({"response_code": 4, "error_msg": "GET REQUEST!"})
 
 
-def get_table(request):
-    if request.method == "POST":
+class GetTableView(View):
+    # @permission_decorator_class_based(token_authenticate,
+    #     {USER_ROLES['CAFE_OWNER']},
+    #     {USER_PLANS_CHOICES['FREE']},
+    #     branch_disable=False)
+    def post(self, request, *args, **kwargs):
         rec_data = json.loads(request.read().decode('utf-8'))
         username = rec_data['username']
         if not request.session.get('is_logged_in', None) == username:
@@ -84,100 +94,108 @@ def get_table(request):
             'table_cat_id': table.category.id,
         }
         return JsonResponse({"response_code": 2, 'table': table_data})
-    return JsonResponse({"response_code": 4, "error_msg": "GET REQUEST!"})
 
 
-def add_table_category(request):
-    if request.method != "POST":
-        return JsonResponse({"response_code": 4, "error_msg": "GET REQUEST!"})
-    rec_data = json.loads(request.read().decode('utf-8'))
-    name = rec_data.get('name')
-    table_cat_id = rec_data.get('id')
-    branch_id = rec_data.get('branch')
+class AddTableCategoryView(View):
+    # @permission_decorator_class_based(token_authenticate,
+    #     {USER_ROLES['CAFE_OWNER']},
+    #     {USER_PLANS_CHOICES['FREE']},
+    #     branch_disable=False)
+    def post(self, request, *args, **kwargs):
+        rec_data = json.loads(request.read().decode('utf-8'))
+        name = rec_data.get('name')
+        table_cat_id = rec_data.get('id')
+        branch_id = rec_data.get('branch')
 
-    if not name or not branch_id:
-        return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
+        if not name or not branch_id:
+            return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
 
-    if table_cat_id == 0:
-        new_table_category = TableCategory(
-            name=name,
-            branch_id=branch_id
-        )
-        try:
-            new_table_category.save()
-        except IntegrityError:
-            return JsonResponse({"response_code": 3, "error_msg": UNIQUE_VIOLATION_ERROR})
+        if table_cat_id == 0:
+            new_table_category = TableCategory(
+                name=name,
+                branch_id=branch_id
+            )
+            try:
+                new_table_category.save()
+            except IntegrityError:
+                return JsonResponse({"response_code": 3, "error_msg": UNIQUE_VIOLATION_ERROR})
 
-        return JsonResponse({"response_code": 2})
-    else:
-        old_table_cat = TableCategory.objects.get(pk=table_cat_id)
-        old_table_cat.name = name
-        try:
-            old_table_cat.save()
-        except IntegrityError:
-            return JsonResponse({"response_code": 3, "error_msg": UNIQUE_VIOLATION_ERROR})
-        return JsonResponse({"response_code": 2})
-
-
-def get_tables(request):
-    if request.method != "POST":
-        return JsonResponse({"response_code": 4, "error_msg": "GET REQUEST!"})
-
-    rec_data = json.loads(request.read().decode('utf-8'))
-    username = rec_data.get('username')
-    branch_id = rec_data.get('branch')
-    if not request.session.get('is_logged_in', None) == username:
-        return JsonResponse({"response_code": 3, "error_msg": UNATHENTICATED})
-
-    tables = Table.objects.filter(category__branch_id=branch_id).order_by('id')
-    tables_data = []
-    for table in tables:
-        tables_data.append({
-            'table_id': table.pk,
-            'table_name': table.name,
-            'table_category_name': table.category.name,
-            'is_checked': 0
-        })
-    return JsonResponse({"response_code": 2, 'tables': tables_data})
+            return JsonResponse({"response_code": 2})
+        else:
+            old_table_cat = TableCategory.objects.get(pk=table_cat_id)
+            old_table_cat.name = name
+            try:
+                old_table_cat.save()
+            except IntegrityError:
+                return JsonResponse({"response_code": 3, "error_msg": UNIQUE_VIOLATION_ERROR})
+            return JsonResponse({"response_code": 2})
 
 
-def get_table_category(request):
-    if request.method != "POST":
-        return JsonResponse({"response_code": 4, "error_msg": "GET REQUEST!"})
+class GetTablesView(View):
+    # @permission_decorator_class_based(token_authenticate,
+    #     {USER_ROLES['CAFE_OWNER']},
+    #     {USER_PLANS_CHOICES['FREE']},
+    #     branch_disable=False)
+    def post(self, request, *args, **kwargs):
+        rec_data = json.loads(request.read().decode('utf-8'))
+        username = rec_data.get('username')
+        branch_id = rec_data.get('branch')
+        if not request.session.get('is_logged_in', None) == username:
+            return JsonResponse({"response_code": 3, "error_msg": UNATHENTICATED})
 
-    rec_data = json.loads(request.read().decode('utf-8'))
-    username = rec_data['username']
-    table_cat_id = rec_data.get('table_cat_id')
-
-    if not request.session.get('is_logged_in', None) == username:
-        return JsonResponse({"response_code": 3, "error_msg": UNATHENTICATED})
-
-    if not table_cat_id:
-        return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
-
-    table_cat = TableCategory.objects.get(pk=table_cat_id)
-    table_cat_data = {
-        'id': table_cat.pk,
-        'name': table_cat.name,
-    }
-    return JsonResponse({"response_code": 2, 'table_category': table_cat_data})
+        tables = Table.objects.filter(category__branch_id=branch_id).order_by('id')
+        tables_data = []
+        for table in tables:
+            tables_data.append({
+                'table_id': table.pk,
+                'table_name': table.name,
+                'table_category_name': table.category.name,
+                'is_checked': 0
+            })
+        return JsonResponse({"response_code": 2, 'tables': tables_data})
 
 
-def get_table_categories(request):
-    if request.method != "POST":
-        return JsonResponse({"response_code": 4, "error_msg": "GET REQUEST!"})
+class GetTableCategoryView(View):
+    # @permission_decorator_class_based(token_authenticate,
+    #     {USER_ROLES['CAFE_OWNER']},
+    #     {USER_PLANS_CHOICES['FREE']},
+    #     branch_disable=False)
+    def post(self, request, *args, **kwargs):
+        rec_data = json.loads(request.read().decode('utf-8'))
+        username = rec_data['username']
+        table_cat_id = rec_data.get('table_cat_id')
 
-    rec_data = json.loads(request.read().decode('utf-8'))
-    username = rec_data.get('username')
-    branch_id = rec_data.get('branch')
-    if not request.session.get('is_logged_in', None) == username:
-        return JsonResponse({"response_code": 3, "error_msg": UNATHENTICATED})
+        if not request.session.get('is_logged_in', None) == username:
+            return JsonResponse({"response_code": 3, "error_msg": UNATHENTICATED})
 
-    table_cats = TableCategory.objects.filter(branch_id=branch_id).order_by('id')
-    table_cats_data = []
-    for cat in table_cats:
-        table_cats_data.append({
-            'id': cat.pk,
-            'name': cat.name,
-        })
-    return JsonResponse({"response_code": 2, 'table_categories': table_cats_data})
+        if not table_cat_id:
+            return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
+
+        table_cat = TableCategory.objects.get(pk=table_cat_id)
+        table_cat_data = {
+            'id': table_cat.pk,
+            'name': table_cat.name,
+        }
+        return JsonResponse({"response_code": 2, 'table_category': table_cat_data})
+
+
+class GetTableCategoriesView(View):
+    # @permission_decorator_class_based(token_authenticate,
+    #     {USER_ROLES['CAFE_OWNER']},
+    #     {USER_PLANS_CHOICES['FREE']},
+    #     branch_disable=False)
+    def post(self, request, *args, **kwargs):
+        rec_data = json.loads(request.read().decode('utf-8'))
+        username = rec_data.get('username')
+        branch_id = rec_data.get('branch')
+        if not request.session.get('is_logged_in', None) == username:
+            return JsonResponse({"response_code": 3, "error_msg": UNATHENTICATED})
+
+        table_cats = TableCategory.objects.filter(branch_id=branch_id).order_by('id')
+        table_cats_data = []
+        for cat in table_cats:
+            table_cats_data.append({
+                'id': cat.pk,
+                'name': cat.name,
+            })
+        return JsonResponse({"response_code": 2, 'table_categories': table_cats_data})
