6b12685d01173dd6836d8437bf2fa74c9156afeb
Sohrab Alimardani <serpent_code@protonmail.com>
2020-09-06 15:55:59 +0430

    class based tokens for LotteryView.py

--- accountiboard/accounti/views/LotteryView.py
+++ accountiboard/accounti/views/LotteryView.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
+from django.views import View
+from accountiboard.custom_permissions import *
 import json, jdatetime
 from accounti.models import *
 from random import randint
@@ -26,112 +28,108 @@ def do_lottery(lot_data):
             return final_winner_card_number
 
 
-def lottery(request):
-    if not request.method == "POST":
-        return JsonResponse({"response_code": 4, "error_msg": "GET REQUEST!"})
+class LotteryView(View):
+    @permission_decorator_class_based(token_authenticate,
+        {USER_ROLES['CAFE_OWNER'], USER_ROLES['MANAGER'], USER_ROLES['CASHIER'], USER_ROLES['ACCOUNTANT']},
+        {USER_PLANS_CHOICES['STANDARDNORMAL'], USER_PLANS_CHOICES['STANDARDBG'], USER_PLANS_CHOICES['ENTERPRISE']})
+    def post(self, request, *args, **kwargs):
+        rec_data = json.loads(request.read().decode('utf-8'))
+        start_date = rec_data.get('start_date')
+        end_date = rec_data.get('end_date')
+        prize = rec_data.get('prize')
+        branch_id = rec_data.get('branch')
+        if not start_date or not end_date or not prize or not branch_id:
+            return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
 
-    rec_data = json.loads(request.read().decode('utf-8'))
-    start_date = rec_data.get('start_date')
-    end_date = rec_data.get('end_date')
-    prize = rec_data.get('prize')
-    branch_id = rec_data.get('branch')
-    if not start_date or not end_date or not prize or not branch_id:
-        return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
+        organization_object = Branch.objects.get(id=branch_id).organization
+        start_date_split = start_date.split('/')
+        start_date_g = jdatetime.date(int(start_date_split[2]), int(start_date_split[1]),
+                                      int(start_date_split[0])).togregorian()
 
-    organization_object = Branch.objects.get(id=branch_id).organization
-    start_date_split = start_date.split('/')
-    start_date_g = jdatetime.date(int(start_date_split[2]), int(start_date_split[1]),
-                                  int(start_date_split[0])).togregorian()
+        end_date_split = end_date.split('/')
+        end_date_g = jdatetime.date(int(end_date_split[2]), int(end_date_split[1]),
+                                    int(end_date_split[0])).togregorian()
 
-    end_date_split = end_date.split('/')
-    end_date_g = jdatetime.date(int(end_date_split[2]), int(end_date_split[1]),
-                                int(end_date_split[0])).togregorian()
+        branches_this_organization = Branch.objects.filter(organization=organization_object)
 
-    branches_this_organization = Branch.objects.filter(organization=organization_object)
+        all_games = Game.objects.filter(branch__in=branches_this_organization, add_date__gte=start_date_g, add_date__lte=end_date_g).order_by('add_date').exclude(
+            member=None)
 
-    all_games = Game.objects.filter(branch__in=branches_this_organization, add_date__gte=start_date_g, add_date__lte=end_date_g).order_by('add_date').exclude(
-        member=None)
+        # all_games = Game.objects.filter(add_date__gte=start_date_g, add_date__lte=end_date_g).order_by('add_date').exclude(
+        #     member=None)
 
-    # all_games = Game.objects.filter(add_date__gte=start_date_g, add_date__lte=end_date_g).order_by('add_date').exclude(
-    #     member=None)
+        lottery_data = []
+        for game in all_games:
+            is_user_exist_lottery_data = False
+            for lot in lottery_data:
+                if game.member.card_number == lot['card_number']:
+                    is_user_exist_lottery_data = True
+                    if game.add_date in lot['days']:
+                        lot['luck_points'] += float(game.points) * lot['multiplier'] / float(16)
+                    else:
+                        if lot['multiplier'] != 3:
+                            lot['multiplier'] += 1
+                        lot['days'].append(game.add_date)
+                        lot['luck_points'] += float(game.points) * lot['multiplier'] / float(16)
+            if not is_user_exist_lottery_data:
+                lottery_data.append({
+                    'card_number': game.member.card_number,
+                    'multiplier': 1,
+                    'days': [game.add_date],
+                    'luck_points': float(game.points) / float(16)
+                })
+        winner = do_lottery(lottery_data)
+        if winner:
+            user = Member.objects.filter(card_number=winner).first()
+            Lottery(user=user, prize=prize, start_date=start_date_g, end_date=end_date_g,
+                              organization=organization_object).save()
+        else:
+            return JsonResponse({"response_code": 3, "error_msg": LOTTERY_CAN_NOT_BE_DONE})
 
-    lottery_data = []
-    for game in all_games:
-        is_user_exist_lottery_data = False
-        for lot in lottery_data:
-            if game.member.card_number == lot['card_number']:
-                is_user_exist_lottery_data = True
-                if game.add_date in lot['days']:
-                    lot['luck_points'] += float(game.points) * lot['multiplier'] / float(16)
-                else:
-                    if lot['multiplier'] != 3:
-                        lot['multiplier'] += 1
-                    lot['days'].append(game.add_date)
-                    lot['luck_points'] += float(game.points) * lot['multiplier'] / float(16)
-        if not is_user_exist_lottery_data:
-            lottery_data.append({
-                'card_number': game.member.card_number,
-                'multiplier': 1,
-                'days': [game.add_date],
-                'luck_points': float(game.points) / float(16)
+        return JsonResponse({"response_code": 2})
+
+
+class LotteryListView(View):
+    @permission_decorator_class_based(token_authenticate,
+        {USER_ROLES['CAFE_OWNER'], USER_ROLES['MANAGER'], USER_ROLES['CASHIER'], USER_ROLES['ACCOUNTANT']},
+        {USER_PLANS_CHOICES['STANDARDNORMAL'], USER_PLANS_CHOICES['STANDARDBG'], USER_PLANS_CHOICES['ENTERPRISE']})
+    def post(self, request, *args, **kwargs):
+        rec_data = json.loads(request.read().decode('utf-8'))
+        branch_id = rec_data.get('branch')
+
+        organization_object = Branch.objects.get(id=branch_id).organization
+        all_lot = Lottery.objects.filter(organization=organization_object).order_by("-id")[:100]
+        all_lot_data = []
+        for lot in all_lot:
+            start_date = lot.start_date
+            start_jalali_date = jdatetime.date.fromgregorian(day=start_date.day, month=start_date.month,
+                                                             year=start_date.year)
+            end_date = lot.end_date
+            end_jalali_date = jdatetime.date.fromgregorian(day=end_date.day, month=end_date.month,
+                                                           year=end_date.year)
+            all_lot_data.append({
+                'id': lot.pk,
+                'start_date': start_jalali_date.strftime("%Y/%m/%d"),
+                'end_date': end_jalali_date.strftime("%Y/%m/%d"),
+                'name': lot.user.first_name + " " + lot.user.last_name,
+                'prize': lot.prize,
+                'is_give': lot.is_give_prize
             })
-    winner = do_lottery(lottery_data)
-    if winner:
-        user = Member.objects.filter(card_number=winner).first()
-        Lottery(user=user, prize=prize, start_date=start_date_g, end_date=end_date_g,
-                          organization=organization_object).save()
-    else:
-        return JsonResponse({"response_code": 3, "error_msg": LOTTERY_CAN_NOT_BE_DONE})
 
-    return JsonResponse({"response_code": 2})
+        return JsonResponse({"response_code": 2, "lotteries": all_lot_data})
 
 
-def lottery_list(request):
-    if not request.method == "POST":
-        return JsonResponse({"response_code": 4, "error_msg": "GET REQUEST!"})
+class GivePrizeView(View):
+    @permission_decorator_class_based(token_authenticate,
+        {USER_ROLES['CAFE_OWNER'], USER_ROLES['MANAGER'], USER_ROLES['CASHIER'], USER_ROLES['ACCOUNTANT']},
+        {USER_PLANS_CHOICES['STANDARDNORMAL'], USER_PLANS_CHOICES['STANDARDBG'], USER_PLANS_CHOICES['ENTERPRISE']},
+        branch_disable=True)
+    def post(self, request, *args, **kwargs):
+        rec_data = json.loads(request.read().decode('utf-8'))
+        lottery_id = rec_data.get('lottery_id')
 
-    rec_data = json.loads(request.read().decode('utf-8'))
-    username = rec_data.get('username')
-    branch_id = rec_data.get('branch')
+        lot_obj = Lottery.objects.filter(pk=lottery_id).first()
+        lot_obj.is_give_prize = 1
+        lot_obj.save()
 
-    if not request.session.get('is_logged_in', None) == username:
-        return JsonResponse({"response_code": 3, "error_msg": UNATHENTICATED})
-
-    organization_object = Branch.objects.get(id=branch_id).organization
-    all_lot = Lottery.objects.filter(organization=organization_object).order_by("-id")[:100]
-    all_lot_data = []
-    for lot in all_lot:
-        start_date = lot.start_date
-        start_jalali_date = jdatetime.date.fromgregorian(day=start_date.day, month=start_date.month,
-                                                         year=start_date.year)
-        end_date = lot.end_date
-        end_jalali_date = jdatetime.date.fromgregorian(day=end_date.day, month=end_date.month,
-                                                       year=end_date.year)
-        all_lot_data.append({
-            'id': lot.pk,
-            'start_date': start_jalali_date.strftime("%Y/%m/%d"),
-            'end_date': end_jalali_date.strftime("%Y/%m/%d"),
-            'name': lot.user.first_name + " " + lot.user.last_name,
-            'prize': lot.prize,
-            'is_give': lot.is_give_prize
-        })
-
-    return JsonResponse({"response_code": 2, "lotteries": all_lot_data})
-
-
-def give_prize(request):
-    if not request.method == "POST":
-        return JsonResponse({"response_code": 4, "error_msg": "GET REQUEST!"})
-
-    rec_data = json.loads(request.read().decode('utf-8'))
-    username = rec_data['username']
-    lottery_id = rec_data['lottery_id']
-
-    if not request.session.get('is_logged_in', None) == username:
-        return JsonResponse({"response_code": 3, "error_msg": UNATHENTICATED})
-
-    lot_obj = Lottery.objects.filter(pk=lottery_id).first()
-    lot_obj.is_give_prize = 1
-    lot_obj.save()
-
-    return JsonResponse({"response_code": 2})
+        return JsonResponse({"response_code": 2})
