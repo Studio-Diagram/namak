ac5d2d03583334a1eab47fbda725230fd9c9deb4
Sohrab Alimardani <serpent_code@protonmail.com>
2020-09-06 11:17:56 +0430

    class based tokens for CashView.py

--- accountiboard/accounti/views/CashView.py
+++ accountiboard/accounti/views/CashView.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
+from django.views import View
+from accountiboard.custom_permissions import *
 import json, jdatetime
 from datetime import datetime
 from accounti.models import *
@@ -7,64 +9,63 @@ from functools import wraps
 from accountiboard.constants import *
 
 
-def get_all_cash(request):
-    if request.method != "POST":
-        return JsonResponse({"response_code": 4, "error_msg": "GET REQUEST!"})
-    rec_data = json.loads(request.read().decode('utf-8'))
-    branch_id = rec_data['branch_id']
-    username = rec_data['username']
+class GetAllCashView(View):
+    @permission_decorator_class_based(token_authenticate,
+        {USER_ROLES['CAFE_OWNER'], USER_ROLES['MANAGER'], USER_ROLES['CASHIER'], USER_ROLES['ACCOUNTANT']},
+        {USER_PLANS_CHOICES['STANDARDNORMAL'], USER_PLANS_CHOICES['STANDARDBG'], USER_PLANS_CHOICES['ENTERPRISE']})
+    def post(self, request, *args, **kwargs):
+        rec_data = json.loads(request.read().decode('utf-8'))
+        branch_id = rec_data['branch_id']
 
-    if not username:
-        return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
-    if not branch_id:
-        return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
-    if not request.session.get('is_logged_in', None) == username:
-        return JsonResponse({"response_code": 3, "error_msg": UNATHENTICATED})
+        if not branch_id:
+            return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
 
-    branch_obj = Branch.objects.get(pk=branch_id)
-    all_cashes = Cash.objects.filter(branch=branch_obj).order_by('-id')[:100]
+        branch_obj = Branch.objects.get(pk=branch_id)
+        all_cashes = Cash.objects.filter(branch=branch_obj).order_by('-id')[:100]
 
-    all_cashes_json_data = []
+        all_cashes_json_data = []
 
-    for cash in all_cashes:
-        all_invoice_sales_cash_total_income = InvoiceSales.objects.filter(cash_desk=cash, is_deleted=False,
-                                                                          is_settled=True).aggregate(
-            Sum('total_price'))
+        for cash in all_cashes:
+            all_invoice_sales_cash_total_income = InvoiceSales.objects.filter(cash_desk=cash, is_deleted=False,
+                                                                              is_settled=True).aggregate(
+                Sum('total_price'))
 
-        cash_start_date = cash.created_date_time.date()
-        cash_start_jalali_date = jdatetime.date.fromgregorian(day=cash_start_date.day, month=cash_start_date.month,
-                                                              year=cash_start_date.year)
-        cash_start_time = cash.created_date_time.time().strftime("%H:%M")
-        if cash.ended_date_time:
-            cash_end_date = cash.ended_date_time.date()
-            cash_end_jalali_date = jdatetime.date.fromgregorian(day=cash_end_date.day, month=cash_end_date.month,
-                                                                year=cash_end_date.year)
-            cash_end_time = cash.ended_date_time.time().strftime("%H:%M")
-            cash_final_end_date = cash_end_jalali_date.strftime("%Y/%m/%d")
-        else:
-            cash_final_end_date = ''
-            cash_end_time = ''
+            cash_start_date = cash.created_date_time.date()
+            cash_start_jalali_date = jdatetime.date.fromgregorian(day=cash_start_date.day, month=cash_start_date.month,
+                                                                  year=cash_start_date.year)
+            cash_start_time = cash.created_date_time.time().strftime("%H:%M")
+            if cash.ended_date_time:
+                cash_end_date = cash.ended_date_time.date()
+                cash_end_jalali_date = jdatetime.date.fromgregorian(day=cash_end_date.day, month=cash_end_date.month,
+                                                                    year=cash_end_date.year)
+                cash_end_time = cash.ended_date_time.time().strftime("%H:%M")
+                cash_final_end_date = cash_end_jalali_date.strftime("%Y/%m/%d")
+            else:
+                cash_final_end_date = ''
+                cash_end_time = ''
 
-        all_cashes_json_data.append({
-            'id': cash.pk,
-            'start_date': cash_start_jalali_date.strftime("%Y/%m/%d"),
-            'end_date': cash_final_end_date,
-            'start_time': cash_start_time,
-            'end_time': cash_end_time,
-            'is_closed': cash.is_close,
-            'total_income': all_invoice_sales_cash_total_income['total_price__sum'] if
-            all_invoice_sales_cash_total_income['total_price__sum'] else 0
-        })
+            all_cashes_json_data.append({
+                'id': cash.pk,
+                'start_date': cash_start_jalali_date.strftime("%Y/%m/%d"),
+                'end_date': cash_final_end_date,
+                'start_time': cash_start_time,
+                'end_time': cash_end_time,
+                'is_closed': cash.is_close,
+                'total_income': all_invoice_sales_cash_total_income['total_price__sum'] if
+                all_invoice_sales_cash_total_income['total_price__sum'] else 0
+            })
 
-    return JsonResponse({"response_code": 2, 'all_cashes': all_cashes_json_data})
+        return JsonResponse({"response_code": 2, 'all_cashes': all_cashes_json_data})
 
 
-def close_cash(request):
-    if request.method == "POST":
+class CloseCashView(View):
+    @permission_decorator_class_based(token_authenticate,
+        {USER_ROLES['CAFE_OWNER'], USER_ROLES['MANAGER'], USER_ROLES['CASHIER'], USER_ROLES['ACCOUNTANT']},
+        {USER_PLANS_CHOICES['STANDARDNORMAL'], USER_PLANS_CHOICES['STANDARDBG'], USER_PLANS_CHOICES['ENTERPRISE']})
+    def post(self, request, *args, **kwargs):
         now = datetime.now()
         rec_data = json.loads(request.read().decode('utf-8'))
         branch_id = rec_data['branch_id']
-        username = rec_data['username']
         if 'night_report_inputs' not in rec_data.keys():
             night_report_inputs = {
                 "current_money_in_cash": 0,
@@ -75,12 +76,8 @@ def close_cash(request):
         else:
             night_report_inputs = rec_data['night_report_inputs']
 
-        if not username:
-            return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
         if not branch_id:
             return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
-        if not request.session.get('is_logged_in', None) == username:
-            return JsonResponse({"response_code": 3, "error_msg": UNATHENTICATED})
 
         if night_report_inputs['income_report'] == "" or night_report_inputs['outcome_report'] == "" \
                 or night_report_inputs['event_tickets'] == "" or night_report_inputs['current_money_in_cash'] == "":
@@ -93,7 +90,7 @@ def close_cash(request):
             return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
 
         branch_obj = Branch.objects.get(pk=branch_id)
-        user_object = User.objects.filter(phone=username).first()
+        user_object = User.objects.filter(phone=request.payload['sub_phone']).first()
         current_cash = Cash.objects.filter(branch=branch_obj, is_close=0)
 
         if current_cash.count() == 1:
@@ -114,21 +111,17 @@ def close_cash(request):
         else:
             return JsonResponse({"response_code": 3, 'error_msg': DUPLICATE_CASH})
 
-    return JsonResponse({"response_code": 4, "error_msg": "GET REQUEST!"})
 
-
-def open_cash(request):
-    if request.method == "POST":
+class OpenCashView(View):
+    @permission_decorator_class_based(token_authenticate,
+        {USER_ROLES['CAFE_OWNER'], USER_ROLES['MANAGER'], USER_ROLES['CASHIER'], USER_ROLES['ACCOUNTANT']},
+        {USER_PLANS_CHOICES['STANDARDNORMAL'], USER_PLANS_CHOICES['STANDARDBG'], USER_PLANS_CHOICES['ENTERPRISE']})
+    def post(self, request, *args, **kwargs):
         rec_data = json.loads(request.read().decode('utf-8'))
         branch_id = rec_data['branch']
-        username = rec_data['username']
 
-        if not username:
-            return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
         if not branch_id:
             return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
-        if not request.session.get('is_logged_in', None) == username:
-            return JsonResponse({"response_code": 3, "error_msg": UNATHENTICATED})
 
         branch_obj = Branch.objects.get(pk=branch_id)
         new_cash = Cash(branch=branch_obj)
@@ -136,21 +129,17 @@ def open_cash(request):
 
         return JsonResponse({"response_code": 2, "new_cash_id": new_cash.id})
 
-    return JsonResponse({"response_code": 4, "error_msg": "GET REQUEST!"})
 
-
-def check_cash_exist(request):
-    if request.method == "POST":
+class CheckCashExistView(View):
+    @permission_decorator_class_based(token_authenticate,
+        {USER_ROLES['CAFE_OWNER'], USER_ROLES['MANAGER'], USER_ROLES['CASHIER'], USER_ROLES['ACCOUNTANT']},
+        {USER_PLANS_CHOICES['STANDARDNORMAL'], USER_PLANS_CHOICES['STANDARDBG'], USER_PLANS_CHOICES['ENTERPRISE']})
+    def post(self, request, *args, **kwargs):
         rec_data = json.loads(request.read().decode('utf-8'))
         branch_id = rec_data['branch_id']
-        username = rec_data['username']
 
-        if not username:
-            return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
         if not branch_id:
             return JsonResponse({"response_code": 3, "error_msg": DATA_REQUIRE})
-        if not request.session.get('is_logged_in', None) == username:
-            return JsonResponse({"response_code": 3, "error_msg": UNATHENTICATED})
 
         branch_obj = Branch.objects.get(pk=branch_id)
         current_cash = Cash.objects.filter(branch=branch_obj, is_close=0)
@@ -167,5 +156,3 @@ def check_cash_exist(request):
                 return JsonResponse({"response_code": 3, 'error_msg': OLD_CASH, 'error_mode': 'OLD_CASH'})
         if current_cash.count() == 1:
             return JsonResponse({"response_code": 2})
-
-    return JsonResponse({"response_code": 4, "error_msg": "GET REQUEST!"})
