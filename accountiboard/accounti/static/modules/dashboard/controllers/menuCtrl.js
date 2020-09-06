214376e40a5d1c4d36939308390ef0f603acff13
imanshafiei <imanshafiei@ymail.com>
2020-09-06 17:46:26 +0430

    Merge menu category and menu item.

--- accountiboard/accounti/static/modules/dashboard/controllers/menuCtrl.js
+++ accountiboard/accounti/static/modules/dashboard/controllers/menuCtrl.js
@@ -1,7 +1,19 @@
 angular.module("dashboard")
-    .controller("menuItemCtrl", function ($scope, $interval, $rootScope, $filter, $http, $timeout, $window, dashboardHttpRequest) {
+    .controller("menuCtrl", function ($scope, $interval, $rootScope, $filter, $http, $timeout, $window, dashboardHttpRequest) {
         var initialize = function () {
-            $scope.is_in_edit_mode_menu_item = false;
+            $scope.new_menu_category_data = {
+                'menu_category_id': 0,
+                'name': '',
+                'kind': '',
+                'printers_id': [],
+                'branch_id': $rootScope.user_data.branch,
+                'username': $rootScope.user_data.username
+            };
+            $scope.searach_data_menu_category = {
+                'search_word': '',
+                'branch_id': $rootScope.user_data.branch,
+                'username': $rootScope.user_data.username
+            };
             $scope.new_menu_item_data = {
                 'menu_item_id': 0,
                 'name': '',
@@ -14,13 +26,46 @@ angular.module("dashboard")
                 'branch_id': $rootScope.user_data.branch,
                 'username': $rootScope.user_data.username
             };
+            $scope.printers = [];
+            $scope.error_message = "";
+
             $scope.get_menu_item_data($rootScope.user_data);
             $scope.get_menu_category_data($rootScope.user_data);
+            $scope.get_printers_data();
+        };
+
+        $scope.get_printers_data = function () {
+            dashboardHttpRequest.getPrinters($rootScope.user_data)
+                .then(function (data) {
+                    $rootScope.is_page_loading = false;
+                    if (data['response_code'] === 2) {
+                        $scope.printers = data['printers'];
+                    }
+                    else if (data['response_code'] === 3) {
+                        $scope.error_message = data['error_msg'];
+                        $scope.openErrorModal();
+                    }
+                }, function (error) {
+                    $rootScope.is_page_loading = false;
+                    $scope.error_message = error;
+                    $scope.openErrorModal();
+                });
+        };
+
+        $scope.changePrinterCheckBox = function (is_checked, printer_id) {
+            var index_of_printer_id = $scope.new_menu_category_data.printers_id.indexOf(printer_id);
+            if (index_of_printer_id === -1) {
+                $scope.new_menu_category_data.printers_id.push(printer_id);
+            }
+            else {
+                $scope.new_menu_category_data.printers_id.splice(index_of_printer_id, 1);
+            }
         };
 
         $scope.get_menu_category_data = function (data) {
             dashboardHttpRequest.getMenuCategories(data)
                 .then(function (data) {
+                    $rootScope.is_page_loading = false;
                     if (data['response_code'] === 2) {
                         $scope.menu_categories = data['menu_categories'];
                     }
@@ -29,6 +74,7 @@ angular.module("dashboard")
                         $scope.openErrorModal();
                     }
                 }, function (error) {
+                    $rootScope.is_page_loading = false;
                     $scope.error_message = error;
                     $scope.openErrorModal();
                 });
@@ -52,19 +98,107 @@ angular.module("dashboard")
                 });
         };
 
-        $scope.openAddMenuItemModal = function () {
+        $scope.openErrorModal = function () {
             jQuery.noConflict();
             (function ($) {
-                $('#addMenuItemModal').modal('show');
+                $('#errorModal').modal('show');
+                $('#addMenuCategoryModal').css('z-index', 1000);
+                $('#addMenuItemModal').css('z-index', 1000);
             })(jQuery);
         };
 
-        $scope.closeAddMenuItemModal = function () {
+        $scope.closeErrorModal = function () {
             jQuery.noConflict();
             (function ($) {
-                $('#addMenuItemModal').modal('hide');
+                $('#errorModal').modal('hide');
+                $('#addMenuCategoryModal').css('z-index', "");
+                $('#addMenuItemModal').css('z-index', "");
             })(jQuery);
-            $scope.resetFrom();
+        };
+
+        $scope.openAddMenuCategoryModal = function () {
+            jQuery.noConflict();
+            (function ($) {
+                $('#addMenuCategoryModal').modal('show');
+            })(jQuery);
+        };
+
+        $scope.closeAddMenuCategoryModal = function () {
+            jQuery.noConflict();
+            (function ($) {
+                $('#addMenuCategoryModal').modal('hide');
+                $scope.resetFrom();
+            })(jQuery);
+        };
+
+        $scope.addMenuCategory = function () {
+            dashboardHttpRequest.addMenuCategory($scope.new_menu_category_data)
+                .then(function (data) {
+                    if (data['response_code'] === 2) {
+                        $scope.get_menu_category_data($rootScope.user_data);
+                        $scope.resetFrom();
+                        $scope.closeAddMenuCategoryModal();
+                    }
+                    else if (data['response_code'] === 3) {
+                        $scope.error_message = data['error_msg'];
+                        $scope.openErrorModal();
+                    }
+                }, function (error) {
+                    $scope.error_message = error;
+                    $scope.openErrorModal();
+                });
+        };
+
+        $scope.editMenuCategory = function (menu_category_id) {
+            var data = {
+                'username': $rootScope.user_data.username,
+                'branch': $rootScope.user_data.branch,
+                'menu_category_id': menu_category_id
+            };
+            dashboardHttpRequest.getMenuCategory(data)
+                .then(function (data) {
+                    if (data['response_code'] === 2) {
+                        $scope.printers = data['menu_category']['printers'];
+                        $scope.new_menu_category_data = {
+                            'menu_category_id': data['menu_category']['id'],
+                            'name': data['menu_category']['name'],
+                            'kind': data['menu_category']['kind'],
+                            'printers_id': data['menu_category']['printers_id'],
+                            'branch_id': $rootScope.user_data.branch,
+                            'username': $rootScope.user_data.username
+                        };
+                        $scope.openAddMenuCategoryModal();
+                    }
+                    else if (data['response_code'] === 3) {
+                        $scope.error_message = data['error_msg'];
+                        $scope.openErrorModal();
+                    }
+                }, function (error) {
+                    $scope.error_message = error;
+                    $scope.openErrorModal();
+                });
+
+        };
+
+        $scope.change_order = function (menu_cat_id, type) {
+            var sending_data = {
+                "menu_cat_id": menu_cat_id,
+                "change_type": type,
+                "username": $rootScope.user_data.username
+            };
+            dashboardHttpRequest.changeMenuCategoryOrder(sending_data)
+                .then(function (data) {
+                    if (data['response_code'] === 2) {
+                        $scope.get_menu_category_data($rootScope.user_data);
+                    }
+                    else if (data['response_code'] === 3) {
+                        $scope.error_message = data['error_msg'];
+                        $scope.openErrorModal();
+                    }
+                }, function (error) {
+                    $scope.error_message = error;
+                    $scope.openErrorModal();
+                });
         };
 
         $scope.addMenuItem = function () {
@@ -73,7 +207,7 @@ angular.module("dashboard")
                     if (data['response_code'] === 2) {
                         $scope.get_menu_item_data($rootScope.user_data);
                         $scope.resetFrom();
-                        $scope.closeAddMenuItemModal();
+                        $rootScope.close_modal('addMenuItemModal');
                     }
                     else if (data['response_code'] === 3) {
                         $scope.error_message = data['error_msg'];
@@ -108,7 +242,6 @@ angular.module("dashboard")
 
 
         $scope.editMenuItem = function (menu_item_id) {
-            $scope.is_in_edit_mode_menu_item = true;
             var data = {
                 'username': $rootScope.user_data.username,
                 'menu_item_id': menu_item_id
@@ -124,7 +257,7 @@ angular.module("dashboard")
                             'branch_id': $rootScope.user_data.branch,
                             'username': $rootScope.user_data.username
                         };
-                        $scope.openAddMenuItemModal();
+                        $rootScope.open_modal('addMenuItemModal');
                     }
                     else if (data['response_code'] === 3) {
                         $scope.error_message = data['error_msg'];
@@ -174,23 +307,15 @@ angular.module("dashboard")
             })(jQuery);
         };
 
-        $scope.openErrorModal = function () {
-            jQuery.noConflict();
-            (function ($) {
-                $('#errorModal').modal('show');
-                $('#addMenuItemModal').css('z-index', 1000);
-            })(jQuery);
-        };
-
-        $scope.closeErrorModal = function () {
-            jQuery.noConflict();
-            (function ($) {
-                $('#errorModal').modal('hide');
-                $('#addMenuItemModal').css('z-index', "");
-            })(jQuery);
-        };
-
         $scope.resetFrom = function () {
+            $scope.new_menu_category_data = {
+                'menu_category_id': 0,
+                'name': '',
+                'kind': '',
+                'printers_id': [],
+                'branch_id': $rootScope.user_data.branch,
+                'username': $rootScope.user_data.username
+            };
             $scope.new_menu_item_data = {
                 'menu_item_id': 0,
                 'name': '',
