58273e4e33de6d3434de32cac19b774212c2b0cd
Sohrab Alimardani <serpent_code@protonmail.com>
2020-09-08 20:26:38 +0430

    improved menu category ordering performance for the general case

--- accountiboard/accounti/views/MenuCategoryView.py
+++ accountiboard/accounti/views/MenuCategoryView.py
@@ -45,9 +45,25 @@ class ChangeListOrderView(View):
                 all_menu_categories_list[current_menu_cat_index] = all_menu_categories_list[current_menu_cat_index + 1]
                 all_menu_categories_list[current_menu_cat_index + 1] = temp
 
-        for i, menu_cat in enumerate(all_menu_categories_list):
-            menu_cat.list_order = i
-            menu_cat.save()
+        # check if have to reorder all:
+        if len({x.list_order for x in all_menu_categories_list}) < len(all_menu_categories_list):
+            for i, menu_cat in enumerate(all_menu_categories_list):
+                menu_cat.list_order = i
+                menu_cat.save()
+        # otherwise just update the two affected menu_categories:
+        else:
+            if change_type == 'UP':
+                temp = all_menu_categories_list[current_menu_cat_index - 1].list_order
+                all_menu_categories_list[current_menu_cat_index - 1].list_order = all_menu_categories_list[current_menu_cat_index].list_order
+                all_menu_categories_list[current_menu_cat_index].list_order = temp
+                all_menu_categories_list[current_menu_cat_index - 1].save()
+                all_menu_categories_list[current_menu_cat_index].save()
+            elif change_type == 'DOWN':
+                temp = all_menu_categories_list[current_menu_cat_index + 1].list_order
+                all_menu_categories_list[current_menu_cat_index + 1].list_order = all_menu_categories_list[current_menu_cat_index].list_order
+                all_menu_categories_list[current_menu_cat_index].list_order = temp
+                all_menu_categories_list[current_menu_cat_index + 1].save()
+                all_menu_categories_list[current_menu_cat_index].save()
 
         return JsonResponse({})
 
