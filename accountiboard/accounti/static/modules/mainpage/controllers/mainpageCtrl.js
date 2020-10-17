853d9a674dc56df6f3ecdd6eb6068b1cf64c4a21
imanshafiei <imanshafiei@ymail.com>
2020-10-17 12:47:59 +0330

    FInal imrpovements and bug fix.

--- accountiboard/accounti/static/modules/mainpage/controllers/mainpageCtrl.js
+++ accountiboard/accounti/static/modules/mainpage/controllers/mainpageCtrl.js
@@ -1,6 +1,7 @@
 angular.module("mainpage")
     .controller("mainpageCtrl", function ($scope, $interval, $rootScope, $filter, $http, $state, $auth, $timeout, $window, mainpageHttpRequest) {
         var initialize = function () {
+            $state.go('main.login');
         };
 
         initialize();
