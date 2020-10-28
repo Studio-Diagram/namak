26a12b1502309f8b9e452e4a647007c34d34274a
imanshafiei <imanshafiei@ymail.com>
2020-10-28 18:16:10 +0330

    landing improves.

--- accountiboard/accounti/static/modules/mainpage/controllers/mainpageCtrl.js
+++ accountiboard/accounti/static/modules/mainpage/controllers/mainpageCtrl.js
@@ -1,7 +1,7 @@
 angular.module("mainpage")
     .controller("mainpageCtrl", function ($scope, $interval, $rootScope, $filter, $http, $state, $auth, $timeout, $window, mainpageHttpRequest) {
         var initialize = function () {
-            $state.go('main.login');
+            // $state.go('main.login');
         };
 
         initialize();
